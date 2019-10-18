28b2dd07e9e645523454828d55a046378f642a18
paschbaer <paschbaer@gmail.com>
2019-10-18 18:20:53 +0200

    VS Code Project

---
+++ Blink/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.IO.Ports;
+using LattePanda.Firmata;
+
+namespace Blink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Arduino ...");
+
+            //user that runs this program must be added to group 'dialout'
+            //sudo adduser <user> dialout
+            Arduino arduino = new Arduino();
+
+            int pin = 13;   //digital pin 13 is LED
+            arduino.pinMode(pin, Arduino.OUTPUT);           //set diqgital pin as output
+            while (true)
+            {
+                arduino.digitalWrite(pin, Arduino.HIGH);    //set LED on
+                Thread.Sleep(500);
+                arduino.digitalWrite(pin, Arduino.LOW);     //set LED off
+                Thread.Sleep(500);
+            }
+
+        }
+    }
+}
